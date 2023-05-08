8605799e2565ebd7224a96ac9a97012c0081c34e
Sergiy-Galenko <131354874+Sergiy-Galenko@users.noreply.github.com>
2023-05-08 17:28:00 +0300

    Nik test

--- main.py
+++ main.py
@@ -1,65 +1,40 @@
-import gettext
-import logging
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, CallbackContext
+import requests
+from bs4 import BeautifulSoup
 
-# Встановіть свій токен, який вам надав BotFather
-TOKEN = "YOUR_TELEGRAM_BOT_TOKEN"
+def get_movie_info(search_query):
+    # Замените на URL сайта, с которым вы хотите работать
+    base_url = 'https://rezka.ag/'
+    search_url = f"{base_url}?q={search_query}"
+    
+    response = requests.get(search_url)
+    
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        
+        # Замените на соответствующий селектор для названия фильма и изображения
+        movie_title_selector = 'div.movie-title'
+        movie_image_selector = 'img.movie-image'
+        
+        movie_title = soup.select_one(movie_title_selector)
+        movie_image = soup.select_one(movie_image_selector)
+        
+        if movie_title and movie_image:
+            return {
+                'title': movie_title.text.strip(),
+                'image': movie_image['src']
+            }
+        else:
+            return None
+    else:
+        print(f"Error: {response.status_code}")
+        return None
 
-
-def start(update: Update, context: CallbackContext):
-    keyboard = [
-        [
-            InlineKeyboardButton("Оберіть мову", callback_data="button1"),
-            InlineKeyboardButton("Кнопка 1", callback_data="button2"),
-            InlineKeyboardButton("Кнопка 2", callback_data="button3"),
-            InlineKeyboardButton("Аніме", callback_data="button4"),
-        ]
-    ]
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    update.message.reply_text("Виберіть кнопку:", reply_markup=reply_markup)
-
-def button_callback(update: Update, context: CallbackContext):
-    query = update.callback_query
-    query.answer()
-
-    if query.data == "button1":
-        response_text = "Ви натиснули кнопку 1."
-    elif query.data == "button2":
-        response_text = "Ви натиснули кнопку 2."
-    elif query.data == "button3":
-        response_text = "Ви натиснули кнопку Аніме."
-
-    query.edit_message_text(text=response_text)
-
-def language(update: Update, context: CallbackContext):
-
-    lang_code = update.effective_user.language_code
-
-    lang = gettext.translation("messages", "locale", [lang_code], fallback=True)
-    _ = lang.gettext
-
-    lang_keyboard = [
-        [
-            InlineKeyboardButton("🇺🇸 English", callback_data="en"),
-            InlineKeyboardButton("🇺🇦 Українська", callback_data="uk"),
-        ]
-    ]
-
-
-def main():
-    updater = Updater(TOKEN, use_context=True)
-
-    dispatcher = updater.dispatcher
-    dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(CallbackQueryHandler(button_callback))
-
-    updater.start_polling()
-    updater.idle()
-
-if __name__ == "__main__":
-    main()
-
-def sum():
-    pass
-
+if __name__ == '__main__':
+    search_query = input('Введите название фильма: ')
+    movie_info = get_movie_info(search_query)
+    
+    if movie_info:
+        print(f"Название фильма: {movie_info['title']}")
+        print(f"Картинка фильма: {movie_info['image']}")
+    else:
+        print("Фильм не найден.")
