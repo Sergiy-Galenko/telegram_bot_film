80923c3b05509922d7eda55e35cc90238dd4da8d
Sergiy-Galenko <131354874+Sergiy-Galenko@users.noreply.github.com>
2023-05-08 22:19:59 +0300

    up

--- Button/newBotton.py
+++ Button/newBotton.py
@@ -1,31 +1,41 @@
-# newButton
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Updater, CallbackQueryHandler, CommandHandler
 
+# Обработчик команды "/start"
 def start(update, context):
-    keyboard = [[InlineKeyboardButton("1", callback_data='1'),
-                 InlineKeyboardButton("2", callback_data='2')]]
-
+    # Создаем клавиатуру с двумя кнопками
+    keyboard = [
+        [InlineKeyboardButton("1", callback_data='1'),
+         InlineKeyboardButton("2", callback_data='2')]
+    ]
     reply_markup = InlineKeyboardMarkup(keyboard)
 
+    # Отправляем сообщение с вариантами выбора
     update.message.reply_text('Please choose:', reply_markup=reply_markup)
 
+# Обработчик нажатия на кнопку
 def button(update, context):
     query = update.callback_query
     query.answer()
+    # Определяем, на какую кнопку нажал пользователь
     if query.data == '1':
         query.message.reply_text('Привет')
     elif query.data == '2':
         query.message.reply_text('Пока')
 
+# Основная функция
 def main():
-    updater = Updater('5499590162:AAFyno0cbsJw12j_mfftTuX9dxebttvJPEM', use_context=True)
+    # Создаем экземпляр Updater и передаем токен бота
+    updater = Updater('YOUR_TOKEN_HERE', use_context=True)
 
+    # Получаем диспетчер для регистрации обработчиков
     dp = updater.dispatcher
 
+    # Регистрируем обработчики команды /start и нажатия на кнопку
     dp.add_handler(CommandHandler('start', start))
     dp.add_handler(CallbackQueryHandler(button))
 
+    # Запускаем бота
     updater.start_polling()
     updater.idle()
 
