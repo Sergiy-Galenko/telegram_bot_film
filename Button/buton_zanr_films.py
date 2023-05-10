54c02a30710d841b3c6eb68aa65cc0ccb43f19e1
zombiks228 <tehno356630100207723@gmail.com>
2023-05-10 22:38:35 +0300

    upbate buton

--- Button/buton_zanr_films.py
+++ Button/buton_zanr_films.py
@@ -16,7 +16,26 @@ def film(update: Update, context: CallbackContext) -> None:
                     [InlineKeyboardButton("Драми", callback_data='Драми'), 
                     InlineKeyboardButton("Фантастика", callback_data='Фантастика')],
                     [InlineKeyboardButton("Трилери", callback_data='Трилери'), 
-                    InlineKeyboardButton("Бойовики", callback_data='Бойовики')]]
+                    InlineKeyboardButton("Бойовики", callback_data='Бойовики')]
+                    [InlineKeyboardButton("Історичний", callback_data="Історичний"), 
+                    InlineKeyboardButton("Бойовик", callback_data="Бойовик")]
+                    [InlineKeyboardButton("Вестерн", callback_data="Вестерн"),
+                    InlineKeyboardButton("Військовий", callback_data="Військовий")]
+                    [InlineKeyboardButton("Детектив", callback_data="Детектив"),
+                    InlineKeyboardButton("Документальний", callback_data="Документальний")]
+                    [InlineKeyboardButton("Драма", callback_data="Драма"),
+                    InlineKeyboardButton("Жахи", callback_data="Жахи")]
+                    [InlineKeyboardButton("Комедія", callback_data="Комедія"),
+                    InlineKeyboardButton("Кримінал", callback_data="Кримінал")]
+                    [InlineKeyboardButton("Мелодрама", callback_data="Мелодрама"),
+                    InlineKeyboardButton("Музика", callback_data="Музика")]
+                    [InlineKeyboardButton("Мультфільм", callback_data="Мультфільм"),
+                    InlineKeyboardButton("Пригоди", callback_data="Пригоди")]
+                    [InlineKeyboardButton("Сімейний", callback_data="Сімейний"),
+                    InlineKeyboardButton("Телефільм", callback_data="Телефільм")]
+                    [InlineKeyboardButton("Трилер", callback_data="Трилер"),
+                    InlineKeyboardButton("Фантастика", callback_data="Фантастика")]
+                    [InlineKeyboardButton("Фентезі", callback_data="Фентезі")]]
 
     reply_markup = InlineKeyboardMarkup(keyboard)
     update.message.reply_text('Виберіть жанр фільму:', reply_markup=reply_markup)
@@ -36,27 +55,6 @@ def main() -> None:
 
 if __name__ == '__main__':
     main()
-
-#українські назви
-historical_button = InlineKeyboardButton("Історичний", callback_data="historical")
-action_button = InlineKeyboardButton("Бойовик", callback_data="action")
-western_button = InlineKeyboardButton("Вестерн", callback_data="western")
-military_button = InlineKeyboardButton("Військовий", callback_data="military")
-detective_button = InlineKeyboardButton("Детектив", callback_data="detective")
-documentary_button = InlineKeyboardButton("Документальний", callback_data="documentary")
-drama_button = InlineKeyboardButton("Драма", callback_data="drama")
-horror_button = InlineKeyboardButton("Жахи", callback_data="horror")
-comedy_button = InlineKeyboardButton("Комедія", callback_data="comedy")
-crime_button = InlineKeyboardButton("Кримінал", callback_data="crime")
-melodrama_button = InlineKeyboardButton("Мелодрама", callback_data="melodrama")
-music_button = InlineKeyboardButton("Музика", callback_data="music")
-animation_button = InlineKeyboardButton("Мультфільм", callback_data="animation")
-adventure_button = InlineKeyboardButton("Пригоди", callback_data="adventure")
-family_button = InlineKeyboardButton("Сімейний", callback_data="family")
-tv_movie_button = InlineKeyboardButton("Телефільм", callback_data="tv_movie")
-thriller_button = InlineKeyboardButton("Трилер", callback_data="thriller")
-fantastic_button = InlineKeyboardButton("Фантастика", callback_data="fantastic")
-fantasy_button = InlineKeyboardButton("Фентезі", callback_data="fantasy")
 
 #англійскі назви
 action_button = InlineKeyboardButton("Action", callback_data="action")
