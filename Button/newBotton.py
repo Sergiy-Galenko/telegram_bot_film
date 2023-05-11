bd85e0e0031c2a46e4097ff4ceea7eefb3153d37
rio1111111 <nmirosnicenko544@gmail.com>
2023-05-11 12:19:58 +0300

    U

--- Button/newBotton.py
+++ Button/newBotton.py
@@ -2,9 +2,12 @@
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Updater, CallbackQueryHandler, CommandHandler
 
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+
 def start(update, context):
     keyboard = [[InlineKeyboardButton("1", callback_data='1'),
-                 InlineKeyboardButton("2", callback_data='2')]]
+                 InlineKeyboardButton("2", callback_data='2')],
+                [InlineKeyboardButton("Вибрати колір", callback_data='color')]]
 
     reply_markup = InlineKeyboardMarkup(keyboard)
 
@@ -13,18 +16,36 @@ def start(update, context):
 def button(update, context):
     query = update.callback_query
     query.answer()
+
     if query.data == '1':
         query.message.reply_text('Привет')
     elif query.data == '2':
         query.message.reply_text('Пока')
+    elif query.data == 'color':
+        color_keyboard = [[InlineKeyboardButton("Червоний", callback_data='red'),
+                           InlineKeyboardButton("Синій", callback_data='blue')]]
+
+        color_markup = InlineKeyboardMarkup(color_keyboard)
+
+        query.message.reply_text('Виберіть колір:', reply_markup=color_markup)
+
+def color_button(update, context):
+    query = update.callback_query
+    query.answer()
+
+    if query.data == 'red':
+        query.message.reply_text('Ви вибрали червоний колір')
+    elif query.data == 'blue':
+        query.message.reply_text('Ви вибрали синій колір')
 
 def main():
-    updater = Updater('5499590162:AAFyno0cbsJw12j_mfftTuX9dxebttvJPEM', use_context=True)
+    updater = Updater('TOKEN')
 
     dp = updater.dispatcher
 
     dp.add_handler(CommandHandler('start', start))
     dp.add_handler(CallbackQueryHandler(button))
+    dp.add_handler(CallbackQueryHandler(color_button))
 
     updater.start_polling()
     updater.idle()
