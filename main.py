51e39867e70716c51ee6efcceeaa62a0a217e876
Sergiy-Galenko <131354874+Sergiy-Galenko@users.noreply.github.com>
2023-05-16 19:58:10 +0300

    up

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 TOKEN = "5845703570:AAFlOF_HbqpJtWfrplzbpBIh0lpmCyucPHo"
 
-
 class BotHandler:
     def __init__(self, token):
         self.updater = Updater(token, use_context=True)
@@ -33,54 +32,33 @@ class BotHandler:
         query = update.callback_query
         query.answer()
         genre = query.data
-        query.edit_message_text(text=f"Ви обрали жанр: {genre}")
 
-
-class BotHandlerSerials:
-    def __init__(self, token):
-        self.updater = Updater(token, use_context=True)
-        self.updater.dispatcher.add_handler(CommandHandler('serials', self.serials))
-        self.updater.dispatcher.add_handler(CallbackQueryHandler(self.button))
-
-    def start(self) -> None:
-        self.updater.start_polling()
-        self.updater.idle()
+        if genre == 'Фильмы':
+            self.serials(update, context)  # if 'Фильмы' is chosen, show the serials menu
+        else:
+            query.edit_message_text(text=f"Ви обрали жанр: {genre}")
 
     def serials(self, update: Update, context: CallbackContext) -> None:
         keyboard = [[InlineKeyboardButton("Вестерн", callback_data="western"),
-                     InlineKeyboardButton("Детектив", callback_data="detective")],
+                    InlineKeyboardButton("Детектив", callback_data="detective")],
                     [InlineKeyboardButton("Дитячий", callback_data="children"),
-                     InlineKeyboardButton("Документальний", callback_data="documentary")],
+                    InlineKeyboardButton("Документальний", callback_data="documentary")],
                     [InlineKeyboardButton("Драма", callback_data="drama"),
-                     InlineKeyboardButton("Екшн і Пригоди", callback_data="action_adventure")],
+                    InlineKeyboardButton("Екшн і Пригоди", callback_data="action_adventure")],
                     [InlineKeyboardButton("Комедія", callback_data="comedy"),
-                     InlineKeyboardButton
-
-                     ("Кримінал", callback_data="crime")],
+                    InlineKeyboardButton("Кримінал", callback_data="crime")],
                     [InlineKeyboardButton("Мильна опера", callback_data="soap_opera"),
-                     InlineKeyboardButton("Мультфільм", callback_data="animation")],
+                    InlineKeyboardButton("Мультфільм", callback_data="animation")],
                     [InlineKeyboardButton("Науково фантастичний", callback_data="sci_fi"),
-                     InlineKeyboardButton("Новини", callback_data="news")],
+                    InlineKeyboardButton("Новини", callback_data="news")],
                     [InlineKeyboardButton("Політика та війна", callback_data="politics_war"),
-                     InlineKeyboardButton("Реаліті-шоу", callback_data="reality_show")],
+                    InlineKeyboardButton("Реаліті-шоу", callback_data="reality_show")],
                     [InlineKeyboardButton("Сімейний", callback_data="family"),
-                     InlineKeyboardButton("Ток-шоу", callback_data="talk_show")]]
+                    InlineKeyboardButton("Ток-шоу", callback_data="talk_show")]]
 
         reply_markup = InlineKeyboardMarkup(keyboard)
         update.message.reply_text('Виберіть жанр серіалу:', reply_markup=reply_markup)
 
-    def button(self, update: Update, context: CallbackContext) -> None:
-        query = update.callback_query
-        query.answer()
-        genre = query.data
-        query.edit_message_text(text=f"Ви обрали жанр: {genre}")
-
-
 if __name__ == '__main__':
     bot = BotHandler(TOKEN)
     bot.start()
-
-    bot_serials = BotHandlerSerials(TOKEN)
-    bot_serials.start()
-
-
