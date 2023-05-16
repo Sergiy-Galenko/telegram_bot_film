0f63072f12cc739c2258b520324d44545542e935
zombiks228 <tehno356630100207723@gmail.com>
2023-05-16 19:13:58 +0300

    updane vibor button

--- Button/vibor.py
+++ Button/vibor.py
@@ -13,8 +13,9 @@ def help(update, context):
 
 def menu(update: Update, context: CallbackContext):
     keyboard = [[InlineKeyboardButton("Сериалы", callback_data='Сериалы'),
-                 InlineKeyboardButton("Фильмы", callback_data='Фильмы')],
-                [InlineKeyboardButton("Аниме", callback_data='Аниме')]]
+                 InlineKeyboardButton("Фильмы", callback_data='Фильмы'),
+                InlineKeyboardButton("Аниме", callback_data='Аниме')],
+                [InlineKeyboardButton("Фільм за описом", callback_data='Фільм за описом')]]
 
     reply_markup = InlineKeyboardMarkup(keyboard)
     update.message.reply_text('Выберите категорию:', reply_markup=reply_markup)
