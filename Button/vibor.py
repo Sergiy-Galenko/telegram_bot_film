4939f0a61bcf4faeb6713be678cb320090ac01ba
Sergiy-Galenko <131354874+Sergiy-Galenko@users.noreply.github.com>
2023-05-16 19:20:27 +0300

    update class

--- Button/vibor.py
+++ Button/vibor.py
@@ -8,30 +8,34 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 TOKEN = "5845703570:AAFlOF_HbqpJtWfrplzbpBIh0lpmCyucPHo"
 
-def help(update, context):
-    update.message.reply_text("Використай команду /menu щоб визвати вікно з вибором того що ти хочеш подивитися.")
+class BotHandler:
+    def __init__(self, token):
+        self.updater = Updater(token, use_context=True)
+        self.updater.dispatcher.add_handler(CommandHandler('menu', self.menu))
+        self.updater.dispatcher.add_handler(CallbackQueryHandler(self.button))
 
-def menu(update: Update, context: CallbackContext):
-    keyboard = [[InlineKeyboardButton("Сериалы", callback_data='Сериалы'),
-                 InlineKeyboardButton("Фильмы", callback_data='Фильмы'),
-                InlineKeyboardButton("Аниме", callback_data='Аниме')],
-                [InlineKeyboardButton("Фільм за описом", callback_data='Фільм за описом')]]
+    def start(self) -> None:
+        self.updater.start_polling()
+        self.updater.idle()
 
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    update.message.reply_text('Выберите категорию:', reply_markup=reply_markup)
+    def help(self, update, context):
+        update.message.reply_text("Використай команду /menu щоб визвати вікно з вибором того що ти хочеш подивитися.")
 
-def button(update: Update, context: CallbackContext) -> None:
-    query = update.callback_query
-    query.answer()
-    genre = query.data
-    query.edit_message_text(text=f"Ви обрали жанр: {genre}")
+    def menu(self, update: Update, context: CallbackContext):
+        keyboard = [[InlineKeyboardButton("Сериалы", callback_data='Сериалы'),
+                     InlineKeyboardButton("Фильмы", callback_data='Фильмы'),
+                    InlineKeyboardButton("Аниме", callback_data='Аниме')],
+                    [InlineKeyboardButton("Фільм за описом", callback_data='Фільм за описом')]]
 
-def main() -> None:
-    updater = Updater(TOKEN, use_context=True)
-    updater.dispatcher.add_handler(CommandHandler('menu', menu))
-    updater.dispatcher.add_handler(CallbackQueryHandler(button))
-    updater.start_polling()
-    updater.idle()
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        update.message.reply_text('Выберите категорию:', reply_markup=reply_markup)
+
+    def button(self, update: Update, context: CallbackContext) -> None:
+        query = update.callback_query
+        query.answer()
+        genre = query.data
+        query.edit_message_text(text=f"Ви обрали жанр: {genre}")
 
 if __name__ == '__main__':
-    main()
+    bot = BotHandler(TOKEN)
+    bot.start()
--- ChatAI/randomSerials/family.py
+++ ChatAI/randomSerials/family.py
@@ -1,28 +1,38 @@
 import requests
 import random
 
-def get_random_family_series(api_key):
-    url = f"https://api.themoviedb.org/3/discover/tv?api_key={api_key}&with_genres=10751"
-    response = requests.get(url)
-    data = response.json()
+class SeriesFetcher:
+    BASE_URL = "https://api.themoviedb.org/3"
+    IMAGE_BASE_URL = "https://image.tmdb.org/t/p/original"
+    SERIES_BASE_URL = "https://www.themoviedb.org/tv"
 
-    if data['results']:
-        random_series = random.choice(data['results'])
-        series_id = random_series['id']
+    def __init__(self, api_key):
+        self.api_key = api_key
 
-        series_details_url = f"https://api.themoviedb.org/3/tv/{series_id}?api_key={api_key}"
-        series_data = requests.get(series_details_url).json()
+    def get_random_family_series(self):
+        url = f"{self.BASE_URL}/discover/tv?api_key={self.api_key}&with_genres=10751"
+        response = requests.get(url)
+        data = response.json()
 
-        return {
-            'title': series_data['name'],
-            'poster': f"https://image.tmdb.org/t/p/original{series_data['poster_path']}",
-            'number_of_episodes': series_data['number_of_episodes'],
-            'url': f"https://www.themoviedb.org/tv/{series_id}"
-        }
+        if data['results']:
+            random_series = random.choice(data['results'])
+            series_id = random_series['id']
 
-    return None
+            series_details_url = f"{self.BASE_URL}/tv/{series_id}?api_key={self.api_key}"
+            series_data = requests.get(series_details_url).json()
+
+            return {
+                'title': series_data['name'],
+                'poster': f"{self.IMAGE_BASE_URL}{series_data['poster_path']}",
+                'number_of_episodes': series_data['number_of_episodes'],
+                'url': f"{self.SERIES_BASE_URL}/{series_id}"
+            }
+
+        return None
 
 api_key = '5c45b86ac58a42d9cfc4d98bedca011d'
-series = get_random_family_series(api_key)
+fetcher = SeriesFetcher(api_key)
+series = fetcher.get_random_family_series()
+
 if series:
     print(f"Title: {series['title']}\nPoster: {series['poster']}\nNumber of episodes: {series['number_of_episodes']}\nURL: {series['url']}")
