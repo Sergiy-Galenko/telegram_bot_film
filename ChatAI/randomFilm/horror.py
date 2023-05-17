887012ab7b396161b3c4fe86410a8360faef72bc
Sergiy-Galenko <131354874+Sergiy-Galenko@users.noreply.github.com>
2023-05-17 19:28:17 +0300

    Update randomFilm

--- ChatAI/randomFilm/horror.py
+++ ChatAI/randomFilm/horror.py
@@ -1,32 +1,32 @@
 import requests
 import random
 
+class Movie:
+    def __init__(self, api_key):
+        self.api_key = api_key
+        self.base_url = "https://api.themoviedb.org/3"
+
+    def get_random_movie(self, genre_id):
+        page = random.randint(1, 500)
+        url = f"{self.base_url}/discover/movie?api_key={self.api_key}&with_genres={genre_id}&page={page}"
+        response = requests.get(url)
+        data = response.json()
+
+        if data['results']:
+            random_movie = random.choice(data['results'])
+            movie_id = random_movie['id']
+
+            return {
+                'title': random_movie['title'],
+                'rating': random_movie['vote_average'],
+                'poster': f"https://image.tmdb.org/t/p/original{random_movie['poster_path']}",
+                'url': f"https://www.themoviedb.org/movie/{movie_id}"
+            }
+
+        return None
+
 api_key = '5c45b86ac58a42d9cfc4d98bedca011d'
-base_url = 'https://api.themoviedb.org/3'
-image_base_url = 'https://image.tmdb.org/t/p/w500'
-tmdb_movie_url = 'https://www.themoviedb.org/movie/'
-genre_id = 27  # ID для жанра ужасов
-page = random.randint(1, 500)  # Выбираем случайную страницу от 1 до 500
-
-url = f'{base_url}/discover/movie?api_key={api_key}&with_genres={genre_id}&page={page}'
-
-response = requests.get(url)
-
-if response.status_code == 200:
-    data = response.json()
-    if data['results']:
-        random_movie = random.choice(data['results'])
-        movie_id = random_movie['id']
-        movie_title = random_movie['title']
-        movie_rating = random_movie['vote_average']
-        movie_poster_path = random_movie['poster_path']
-        movie_poster_url = f"{image_base_url}{movie_poster_path}"
-        movie_page_url = f"{tmdb_movie_url}{movie_id}"
-        print(f"Рандомный фильм из категории ужасов: {movie_title}\n"
-              f"Рейтинг: {movie_rating}\n"
-              f"Ссылка на постер: {movie_poster_url}\n"
-              f"Ссылка на фильм: {movie_page_url}")
-    else:
-        print("Не удалось найти фильмы на данной странице. Попробуйте еще раз.")
-else:
-    print("Не удалось получить данные от TMDb. Проверьте ваш API ключ и соединение.")
+movie = Movie(api_key)
+movie_info = movie.get_random_movie(27)  # 27 is the genre id for Horror Movies
+if movie_info:
+    print(f"Title: {movie_info['title']}\nRating: {movie_info['rating']}\nPoster: {movie_info['poster']}\nURL: {movie_info['url']}")
