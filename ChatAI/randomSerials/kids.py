b815da9d3908514535e6adc761cdeee9a23d0f7b
Sergiy-Galenko <131354874+Sergiy-Galenko@users.noreply.github.com>
2023-08-29 21:54:50 +0300

    Update kids.py

--- ChatAI/randomSerials/kids.py
+++ ChatAI/randomSerials/kids.py
@@ -1,8 +1,10 @@
 import requests, random
 
+
 class RandomKidsSeries:
     def __init__(self, api_key):
         self.api_key = api_key
+
     def get_random_kids_series(self):
         data = requests.get(f"https://api.themoviedb.org/3/discover/tv?api_key={self.api_key}&with_genres=10762").json()
         if data['results']:
@@ -15,6 +17,13 @@ class RandomKidsSeries:
                 'url': f"https://www.themoviedb.org/tv/{series['id']}"
             }
 
+
 api_key = '5c45b86ac58a42d9cfc4d98bedca011d'
-if (series := RandomKidsSeries(api_key).get_random_kids_series()):
-    print(f"Title: {series['title']}\nPoster: {series['poster']}\nNumber of episodes: {series['number_of_episodes']}\nURL: {series['url']}")
+kids_series_instance = RandomKidsSeries(api_key)
+random_kids_series = kids_series_instance.get_random_kids_series()
+
+if random_kids_series:
+    print(f"Title: {random_kids_series['title']}")
+    print(f"Poster: {random_kids_series['poster']}")
+    print(f"Number of episodes: {random_kids_series['number_of_episodes']}")
+    print(f"URL: {random_kids_series['url']}")
